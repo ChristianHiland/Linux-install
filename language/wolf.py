fd382b3ecb70ef613fc8518ada9332604cc43c71
Christian Hiland <hilandchristian112@gmail.com>
2023-02-08 13:36:52 -0500

    Create wolf.py

---
+++ language/wolf.py
@@ -0,0 +1,17 @@
+from time import sleep
+
+class wolfen:
+  def bark(printing):
+    print(printing)
+  def math(one):
+    print(one)
+  def listen(listening):
+    listenting = input(listening)
+    return listenting
+  def test():
+    wolfen.bark("Testing...... Good")
+    wolfen.math(1+1)
+    wolfen.bark("Good")
+  def sit(time):
+    sleep(time)
+    return
